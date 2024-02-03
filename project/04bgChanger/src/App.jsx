57ee8977e12de3165c8affd4dc5a67eeb2a03593
Piyush289kumar <Piyushraikwar289@gmail.com>
2024-02-03 11:39:48 +0530

    Build UI

--- project/04bgChanger/src/App.jsx
+++ project/04bgChanger/src/App.jsx
@@ -8,7 +8,16 @@ function App() {
 
   return (
     <>
-     <h1 className='bg-red-500'>Hello</h1>
+     <div className='bg-slate-800 h-screen w-screen grid place-items-center space-x-1'>
+      <div className=' text-white font-bold mx-5 space-x-6'>
+        <button className="bg-red-500 hover:bg-red-900 border-none" id="one">Red</button>
+        <button className="bg-yellow-500 hover:bg-yellow-900 border-none" id="one">Yellow</button>
+        <button className="bg-green-500 hover:bg-green-900 border-none" id="one">Green</button>
+        <button className="bg-sky-500 hover:bg-sky-900 border-none" id="one">Sky Blue</button>
+        <button className="bg-purple-500 hover:bg-purple-900 border-none" id="one">Purple</button>
+        
+      </div>
+     </div>
     </>
   )
 }
