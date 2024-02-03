cce422688265354b60b64c3213020dff74ea6396
Piyush289kumar <Piyushraikwar289@gmail.com>
2024-02-03 12:07:11 +0530

    BgColor Project Is Done

--- project/04bgChanger/src/App.jsx
+++ project/04bgChanger/src/App.jsx
@@ -4,18 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bgColor, setBgColor] = useState('#212121');
 
   return (
     <>
-     <div className='bg-slate-800 h-screen w-screen grid place-items-center space-x-1'>
-      <div className=' text-white font-bold mx-5 space-x-6 bg-white rounded-lg p-4'>
-        <button className="bg-red-500 hover:bg-red-900 border-none" id="one">Red</button>
-        <button className="bg-yellow-500 hover:bg-yellow-900 border-none" id="one">Yellow</button>
-        <button className="bg-green-500 hover:bg-green-900 border-none" id="one">Green</button>
-        <button className="bg-sky-500 hover:bg-sky-900 border-none" id="one">Sky Blue</button>
-        <button className="bg-purple-500 hover:bg-purple-900 border-none" id="one">Purple</button>
+     <div className='h-screen w-screen duration-500' style={{backgroundColor:bgColor}}>
+      <div className='p-4 fixed bottom-2 flex flex-wrap justify-center inset-x-0 space-x-6 w-auto'>
         
+        <div className='text-white font-bold mx-5 bg-white rounded-lg px-5 py-3 flex flex-wrap gap-4'>
+          <button className="bg-red-500 hover:bg-red-900 border-none shadow-2xl" onClick={()=>setBgColor("red")}>Red</button>
+          <button className="bg-yellow-500 hover:bg-yellow-900 border-none"  onClick={()=>setBgColor("yellow")}>Yellow</button>
+          <button className="bg-green-500 hover:bg-green-900 border-none"  onClick={()=>setBgColor("green")}>Green</button>
+          <button className="bg-sky-500 hover:bg-sky-900 border-none"  onClick={()=>setBgColor("blue")}>Sky Blue</button>
+          <button className="bg-purple-500 hover:bg-purple-900 border-none"  onClick={()=>setBgColor("purple")}>Purple</button>      
+        </div>
       </div>
      </div>
     </>
