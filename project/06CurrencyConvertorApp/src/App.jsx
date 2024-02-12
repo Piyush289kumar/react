899f04d6cda9253c1ee47303905191793b57654b
Piyush289kumar <Piyushraikwar289@gmail.com>
2024-02-12 21:19:09 +0530

    init 06CurrencyConvertorApp

---
+++ project/06CurrencyConvertorApp/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='bg-slate-700 h-screen w-screen p-0 m-0'>
+      <h1 className='bg-red-500 text-4xl'>Piyush Kumar Raikwar</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
