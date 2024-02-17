371c368adeddcd6846807d08c08c0c865279cf11
Piyush289kumar <Piyushraikwar289@gmail.com>
2024-02-17 12:52:30 +0530

    Context Funcations Build

--- project/09Todos/src/App.jsx
+++ project/09Todos/src/App.jsx
@@ -2,33 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodosProvider, useTodos, TodosContext } from './Context'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useTodos([])
+
+  const addTodos = (todo) => {
+    setTodos.map((prev) => [{ id: Date.now(), ...todo }, ...prev])
+  }
+
+  const updatedTodos = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo))
+  }
+
+  const deleteTodos = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id))
+  }
+
+  const todoComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id ? { complete: !prevTodo.complete, ...prevTodo } : prevTodo))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <TodosProvider value={{ addTodos, updatedTodos, deleteTodos, todoComplete, todos }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </TodosProvider>
   )
 }
 
