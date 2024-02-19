53b0d692b4d67e22a0a2d786047957884157056b
Piyush289kumar <Piyushraikwar289@gmail.com>
2024-02-19 21:17:21 +0530

    Application Builded

--- project/10ReduxTodo/src/Components/AddTodo.jsx
+++ project/10ReduxTodo/src/Components/AddTodo.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useDispatch } from "react-redux";
 import { addTodo } from "../Features/Todo/todoSlice";
 
-export default function AddTodo() {
+function AddTodo() {
 
     const [input, setInput] = useState('')
     const dispatch = useDispatch()
 
-    const addTodoHandler = (e) =>{
-        e.preventDefault();
-
+    const addTodoHandler = (e) => {
+        e.preventDefault()
         dispatch(addTodo(input))
         setInput('')
     }
-
 
   return (
     <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
@@ -33,3 +31,5 @@ export default function AddTodo() {
     </form>
   )
 }
+
+export default AddTodo
