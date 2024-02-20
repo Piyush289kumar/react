e5729b92f644585dda18c57a2ef418afefa3cb93
Piyush289kumar <piyushraikwar289@gmail.com>
2024-02-20 09:25:41 +0530

    Create Environment Variable

---
+++ project/MegaProject/config/config.js
@@ -0,0 +1,12 @@
+const config = {
+    
+    appWriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    productId: String(import.meta.env.VITE_PRODUCT_ID),
+    databaseId: String(import.meta.env.VITE_DATABASE_ID),
+    collectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    buketId: String(import.meta.env.VITE_BUKET_ID)
+
+}
+
+
+export default config
