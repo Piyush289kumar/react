9e55ce026d212620b7f9de83d7ac39a445ef42b4
Piyush289kumar <piyushraikwar289@gmail.com>
2024-02-22 20:57:35 +0530

    Build Comman Button

---
+++ project/MegaProject/src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Footer from './Footer/Footer'
+import Container from './container/Container'
+import Logo from "./Logo";
+
+export {
+    Header,
+    LogoutBtn,
+    Footer,
+    Container,
+    Logo
+}
