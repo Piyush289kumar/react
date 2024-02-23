e6132aca33096173f138763cbc8ec569bf6dc8d7
Piyush289kumar <piyushraikwar289@gmail.com>
2024-02-23 17:11:27 +0530

    Build Login.jsx

--- project/MegaProject/src/components/index.js
+++ project/MegaProject/src/components/index.js
@@ -3,11 +3,15 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Footer from './Footer/Footer'
 import Container from './container/Container'
 import Logo from "./Logo";
+import Button from './Button'
+import Input from './Input'
 
 export {
     Header,
     LogoutBtn,
     Footer,
     Container,
-    Logo
+    Logo,
+    Button,
+    Input,
 }
