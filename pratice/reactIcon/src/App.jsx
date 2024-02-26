f409fcf645365d41e19bfdef2ae98fc97b4d1dc1
Piyush289kumar <piyushraikwar289@gmail.com>
2024-02-26 20:28:54 +0530

    React Icons Lybrary

---
+++ pratice/reactIcon/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { FaAccusoft } from "react-icons/fa6";
+import { MdNoAdultContent } from "react-icons/md";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <MdNoAdultContent color='red' fontSize={400} />
+    </>
+  )
+}
+
+export default App
