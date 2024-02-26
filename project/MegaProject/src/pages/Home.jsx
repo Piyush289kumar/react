fc872517c18733fb5d272ee910f45c6a24be58b6
Piyush289kumar <piyushraikwar289@gmail.com>
2024-02-26 10:36:11 +0530

    Build Home Page

---
+++ project/MegaProject/src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import dbservice from '../appwrite/dbconfig'
+import { Container, PostCard } from '../components'
+import { useNavigate } from 'react-router-dom'
+
+function Home() {
+
+    const [posts, setPosts] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        dbservice.getPosts()
+            .then((posts) => {
+                if (posts) {
+                    setPosts(posts.documents)
+                } else {
+                    setPosts([])
+                    navigate('/')
+                }
+            })
+    }, [])
+
+
+    if (posts.length <= 0) {
+        return (
+            <div className='w-full py-8 mt-4 text-center'>
+                <Container>
+                    <div className='flex flex-wrap'>
+                        <div className='p-2 w-full'>
+                            <h1 className='text-2xl font-bold hover:text-gray-500'>
+                                Login to read posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+
+
+
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => {
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    })}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Home
