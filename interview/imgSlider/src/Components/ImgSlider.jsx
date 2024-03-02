99a16f7a0613b761ceceb824fbd3235965283dd4
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-02 12:44:17 +0530

    Project Finish

--- interview/imgSlider/src/Components/ImgSlider.jsx
+++ interview/imgSlider/src/Components/ImgSlider.jsx
@@ -1,51 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight, FaArrowCircleRight } from 'react-icons/fa'
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 function ImgSlider({ url, limit = 5, page = 1 }) {
-    console.log(url);
-
     const [images, setImages] = useState([]);
     const [currentSilder, setCurrentSilder] = useState(0);
     const [loading, setLoading] = useState(false);
     const [errorMsg, setErrorMsg] = useState(null);
 
     function handelValuedecrement() {
-
-
         let tempVal = currentSilder;
-        tempVal = tempVal - 1
+        tempVal = tempVal - 1;
 
         if (tempVal !== -1) {
-            setCurrentSilder(tempVal)
+            setCurrentSilder(tempVal);
         } else {
-            setCurrentSilder(images.length - 1)
+            setCurrentSilder(images.length - 1);
         }
-
-
     }
 
     function handelValueIncrement() {
-        console.log('images.length: ' + images.length);
         let tempVal = currentSilder;
-        tempVal = tempVal + 1
+        tempVal = tempVal + 1;
 
         if (tempVal > images.length - 1) {
-            setCurrentSilder(0)
-        } else { setCurrentSilder(tempVal) }
+            setCurrentSilder(0);
+        } else {
+            setCurrentSilder(tempVal);
+        }
     }
 
     async function fetchImges(getUrl) {
-        console.log(`getUrl : ${getUrl}`);
-        console.log(`fetch : ${getUrl}?page=${page}&limit=${limit}`);
         try {
             setLoading(true);
 
             fetch(`${getUrl}?page=${page}&limit=${limit}`)
-                .then((res) => (res.json()))
+                .then((res) => res.json())
                 .then((data) => {
-                    setImages(data)
-                    setLoading(false)
-                })
+                    setImages(data);
+                    setLoading(false);
+                });
         } catch (error) {
             setLoading(false);
             setErrorMsg(error.message);
@@ -53,7 +46,7 @@ function ImgSlider({ url, limit = 5, page = 1 }) {
     }
 
     useEffect(() => {
-        if (url !== '') {
+        if (url !== "") {
             fetchImges(url);
         }
     }, [url]);
@@ -66,37 +59,43 @@ function ImgSlider({ url, limit = 5, page = 1 }) {
         return <div>{`Error Occurs : ${errorMsg}`}</div>;
     }
 
-    console.log(`current Silder Value Index : ${currentSilder}`);
-    console.log(images);
-
-
-    return <div className="bg-violet-600 h-screen w-full grid place-content-center">
-
-
-
-
-        {
-            images && images.length ?
-
-                images.map((img) => (
-                    <div key={img.id}> {
-                        currentSilder == parseInt(img.id) ?
+    return (
+        <div className="bg-violet-600 h-screen w-full grid place-content-center">
+            {images && images.length
+                ? images.map((img) => (
+                    <div key={img.id}>
+                        {" "}
+                        {currentSilder == parseInt(img.id) ? (
                             <div>
-                                <h1 className="text-white font-bold text-2xl text-center mt-4">Image Index : {parseInt(img.id) + 1}</h1>
-                                <img key={img.id} src={img.download_url} alt={img.author} className="w-52 rounded-md mt-5" />
+                                <h1 className="text-white font-bold text-2xl text-center mt-4">
+                                    Image Index : {parseInt(img.id) + 1}
+                                </h1>
+                                <img
+                                    key={img.id}
+                                    src={img.download_url}
+                                    alt={img.author}
+                                    className="w-52 rounded-md mt-5"
+                                />
                             </div>
-                            : null}
+                        ) : null}
                     </div>
                 ))
+                : null}
 
-                : null
-        }
-
-        <div className=" w-full flex justify-center text-white my-5">
-            <FaArrowAltCircleLeft onClick={handelValuedecrement} size={40} className="mx-4" />
-            <FaArrowAltCircleRight onClick={handelValueIncrement} size={40} className="mx-4" />
+            <div className=" w-full flex justify-center text-white my-5">
+                <FaArrowAltCircleLeft
+                    onClick={handelValuedecrement}
+                    size={40}
+                    className="mx-4"
+                />
+                <FaArrowAltCircleRight
+                    onClick={handelValueIncrement}
+                    size={40}
+                    className="mx-4"
+                />
+            </div>
         </div>
-    </div>;
+    );
 }
 
 export default ImgSlider;
