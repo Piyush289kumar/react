68193ec8ad73da54c645cef10b83cf2d0728cb4b
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-05 15:01:39 +0530

    Project init

---
+++ interview/treeView/src/Components/data.js
@@ -0,0 +1,21 @@
+const menus = [
+    {
+        lable: 'Home',
+        child: [
+            {
+                lable: 'Home 1',
+                child: [
+                    {
+                        lable: 'Home 1'
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        lable: 'Home'
+    },
+    {
+        lable: 'Home'
+    },
+]
