4823000fff64e10075a49165184c31248fcb1720
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-08 21:33:47 +0530

    "Updated App component to add dark mode feature with local storage"

--- interview/DarkMode/src/App.jsx
+++ interview/DarkMode/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const mode = JSON.parse(localStorage.getItem("mode"));
+
+    setDarkMode(mode);
+
+    console.log(`mode : ${mode}`);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("mode", JSON.stringify(darkMode));
+
+    console.log(localStorage.setItem("mode = ", JSON.stringify(darkMode)));
+  }, []);
 
   return (
     <>
-     <h1>sdf</h1>
+      <div
+        className={`w-full h-screen bg-violet-700 ${
+          darkMode && "dark:bg-zinc-900"
+        } grid place-content-center`}
+      >
+        <button
+          onClick={() => {
+            setDarkMode(!darkMode);
+          }}
+          className="bg-green-500 text-zinc-200 px-4 py-2 rounded-lg font-bold"
+        >
+          Dark Mode is : {darkMode ? "Enable" : "Disable"}
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
