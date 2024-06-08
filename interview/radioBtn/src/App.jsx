0331dd7f367b59eef92a6af9ba9d584beade04a2
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-08 21:54:50 +0530

    "Added useEffect hook and updated radio button inputs to set state on click"

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
   const myGames = ['PUBG', "BGMI", "MORTAL KOMBAT"];
   const myDays = ['MONDAY', 'TUESDAY', 'SUNDAY', 'FRIDAY']
-  const [count, setCount] = useState(0)
+  const [games, setGames] = useState('')
+  useEffect(() => {
+    const clikedItem = () => {
+      console.log(`games ${games}`);
+    }
+    clikedItem();
+  }, [games])
   return (
     <>
       <div>
@@ -13,16 +19,18 @@ function App() {
           myGames.map((item, idx) => (
             <span style={{ margin: 10 }}>
               <span key={idx}>{item}</span>
-              <input type="radio" name="mygames" id={idx} value={item} key={idx} />
+              <input type="radio" name="mygames" value={item} onClick={(el) => setGames(el.target.value)} />
             </span>
           ))
         }
+        <hr /><hr />
+        <hr /><hr />
         <hr /><hr />
         {
           myDays.map((item, idx) => (
             <span style={{ margin: 10 }}>
               <span key={idx}>{item}</span>
-              <input type="radio" name="mydays" id={idx} value={item} key={idx} />
+              <input type="radio" name="mydays" value={item} />
             </span>
           ))
         }
