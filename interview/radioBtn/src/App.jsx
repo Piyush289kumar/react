110be8df5b696a0dea9a3e8c239e1cc28f30ff4a
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-10 19:24:52 +0530

    Create a Function

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -3,10 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
-  
+
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
   return (
     <>
-     
+
     </>
   )
 }
