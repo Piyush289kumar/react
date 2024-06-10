be38753d61241dcef41cbedbe926601e42b81248
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-10 19:23:26 +0530

    "Removed radio button code and variables from App.jsx"

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -3,38 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
-  const myGames = ['PUBG', "BGMI", "MORTAL KOMBAT"];
-  const myDays = ['MONDAY', 'TUESDAY', 'SUNDAY', 'FRIDAY']
-  const [games, setGames] = useState('')
-  useEffect(() => {
-    const clikedItem = () => {
-      console.log(`games ${games}`);
-    }
-    clikedItem();
-  }, [games])
+  
   return (
     <>
-      <div>
-        {
-          myGames.map((item, idx) => (
-            <span style={{ margin: 10 }}>
-              <span key={idx}>{item}</span>
-              <input type="radio" name="mygames" value={item} onClick={(el) => setGames(el.target.value)} />
-            </span>
-          ))
-        }
-        <hr /><hr />
-        <hr /><hr />
-        <hr /><hr />
-        {
-          myDays.map((item, idx) => (
-            <span style={{ margin: 10 }}>
-              <span key={idx}>{item}</span>
-              <input type="radio" name="mydays" value={item} />
-            </span>
-          ))
-        }
-      </div>
+     
     </>
   )
 }
