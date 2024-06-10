110c3c977d5f5c3c2ce10c53d4e06a9a0e205936
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-10 19:30:30 +0530

    "Removed unnecessary blank lines in App.jsx"

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -3,13 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
-
   const [count, setCount] = useState(0)
-
   useEffect(() => {
     console.log('Component rendered successfully');
   }, [])
-
   return (
     <>
       <div>
