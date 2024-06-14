0d65a46c985d2cf30884b9a97b9d350facfa4ee6
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-14 21:29:49 +0530

    Toggle Button Start

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -3,16 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
-  const [count, setCount] = useState(0)
-  useEffect(() => {
-    console.log('Component rendered successfully');
-  }, [])
+  
+  const 
   return (
     <>
-      <div>
-        <button onClick={() => setCount(count + 1)}>Click Me</button>
-        <p>You Clike {count} Times</p>
-      </div>
+      
     </>
   )
 }
