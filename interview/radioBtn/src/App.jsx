67c73016612e848505d22628000a0e8f7484e36c
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-14 21:30:56 +0530

    "Added state variable and checkbox input to App component"

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -3,11 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
-  
-  const 
+
+  const [isCliked, setisCliked] = useState(false)
   return (
     <>
-      
+
+      <div>
+
+        <h1>Toggle : {isCliked}</h1>
+        <br />
+        <br />
+        <br />
+        <input type="checkbox" name="" id="" />
+
+      </div>
+
     </>
   )
 }
