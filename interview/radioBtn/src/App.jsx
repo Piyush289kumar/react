6cb17588bbdcc1c3bf6bba61309318122d498780
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-14 21:36:50 +0530

    "Updated App.jsx: changed h1 text to display 'ON' or 'OFF' based on state, and modified checkbox onChange event to toggle state"

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -5,16 +5,18 @@ import './App.css'
 function App() {
 
   const [isCliked, setisCliked] = useState(false)
+
+
   return (
     <>
 
       <div>
 
-        <h1>Toggle : {isCliked}</h1>
+        <h1>Toggle : {isCliked ? "ON" : "OFF"}</h1>
         <br />
         <br />
         <br />
-        <input type="checkbox" name="check" id="chekcBox" onChange={() => console.log('change')} />
+        <input type="checkbox" name="check" id="chekcBox" onChange={(prev) => { setisCliked(!isCliked) }} />
 
       </div>
 
