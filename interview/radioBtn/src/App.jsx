bcbfc75471f86792695e2edff69de774a086806e
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-14 21:39:09 +0530

    "Removed unnecessary whitespace and blank lines in App.jsx"

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -3,23 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
-
   const [isCliked, setisCliked] = useState(false)
-
-
   return (
     <>
-
       <div>
-
         <h1>Toggle : {isCliked ? "ON" : "OFF"}</h1>
         <br />
         <br />
         <br />
-        <input type="checkbox" name="check" id="chekcBox" onChange={(prev) => { setisCliked(!isCliked) }} />
-
+        <input type="checkbox" name="check" id="chekcBox" onChange={(prev) => { setisCliked(!isCliked) }} style={{ height: 250, width: 500 }} />
       </div>
-
     </>
   )
 }
