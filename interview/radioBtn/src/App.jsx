32f1b49fc8e9818849fa8851533290a4c76243ef
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-19 21:51:49 +0530

    "Added default value to inputColor state, added div to display inputColor, and updated input field onChange handler"

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
-
-  const [inputColor, setInputColor] = useState('');
-
+  const [inputColor, setInputColor] = useState('red');
   return (
     <>
+      <div style={{ width: 250, height: 250, background: inputColor }}></div>
+      <br />
+      <br />
       <div>
-        <input type="text" name="inputColor" id="inputColor" onChange={(e)=>setInputColor(e.target.value)} />
+        <input type="text" name="inputColor" id="inputColor" onChange={(e) => setInputColor(e.target.value)} />
       </div>
     </>
   )
