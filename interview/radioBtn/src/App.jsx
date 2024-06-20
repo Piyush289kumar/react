c213ef72a4dca85bee7ecfcdcab460023a2319c8
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-20 21:43:05 +0530

    "Added state variables for color codes, updated generateNewColor function to set these states, and displayed color codes in the UI."

--- interview/radioBtn/src/App.jsx
+++ interview/radioBtn/src/App.jsx
@@ -4,31 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
   const [inputColor, setInputColor] = useState('red');
-
+  const [firstColorCode, setFirstColorCode] = useState('');
+  const [secondColorCode, setSecondColorCode] = useState('');
+  const [thirdColorCode, setThirdColorCode] = useState('');
   const randomNumberGenerate = (percentage = 100) => {
     const randomColorR = Math.floor(((Math.random() * 254) + 1))
     const randomColorG = Math.floor(((Math.random() * 254) + 1))
     const randomColorB = Math.floor(((Math.random() * 254) + 1))
-
     return `rgb(${randomColorR},${randomColorG},${randomColorB}) ${percentage}%`;
   }
-
   function generateNewColor() {
-    const generateNewColorCode = `linear-gradient(90deg, ${randomNumberGenerate(0)}, ${randomNumberGenerate(35)}, ${randomNumberGenerate(100)})`;
+    setFirstColorCode(randomNumberGenerate(0));
+    setSecondColorCode(randomNumberGenerate(35));
+    setThirdColorCode(randomNumberGenerate(100));
+    const generateNewColorCode = `linear-gradient(90deg, ${firstColorCode}, ${secondColorCode}, ${thirdColorCode})`;
     console.log('generateNewColorCode', generateNewColorCode);
     setInputColor(generateNewColorCode);
   }
-
-
-
-
   return (
     <>
-      <div style={{ width: 250, height: 250, background: inputColor }}></div>
+      <div style={{ width: 250, height: 250, background: inputColor, borderRadius: 24 }}></div>
       <br />
       <br />
       <div>
-        <button onClick={() => generateNewColor}>Click Me</button>
+        <dir>
+          <p>{`${firstColorCode} -> ${secondColorCode} -> ${thirdColorCode}`}</p>
+        </dir>
+        <button onClick={() => generateNewColor()}>Click Me</button>
         {/* <input type="text" name="inputColor" id="inputColor" onChange={(e) => setInputColor(e.target.value)} /> */}
       </div>
     </>
