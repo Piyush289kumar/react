cdcc6e6c908f2a37354f754008ee851ae46fe378
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-25 21:13:23 +0530

    Server Is Live

--- pratice/todo_list/backend/index.js
+++ pratice/todo_list/backend/index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Server Is Running");
-// });
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ msg: "Sever is Live" });
+});
 
 app.listen(port, (error) => {
   if (!error) {
