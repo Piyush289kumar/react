891f8fe0671caa90ce233c407bc98156cb274333
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-25 20:36:26 +0530

    React State Management Move to Redux Toolkit

--- pratice/todo_list/src/componets/TodoBox.jsx
+++ pratice/todo_list/src/componets/TodoBox.jsx
@@ -1,60 +1,28 @@
 import React, { useState, useEffect } from "react";
+
+import { useSelector, useDispatch } from 'react-redux'
+import { addTaskHandler, checkTaskHandler, deleteTask } from '../features/data/dataSlice'
+
+
 function TodoBox() {
-  const [data, setData] = useState([]);
+
   const [taskInputText, setTaskInputText] = useState("");
-  useEffect(() => {
-    const apiResponse = [
-      {
-        id: 1,
-        task: "Task One",
-        isCompleted: false,
-        isActive: true,
-      },
-      {
-        id: 2,
-        task: "Task Two",
-        isCompleted: false,
-        isActive: true,
-      },
-      {
-        id: 3,
-        task: "Task Three",
-        isCompleted: false,
-        isActive: true,
-      },
-    ];
-    setData(apiResponse);
-  }, []);
-  const addTaskHandler = () => {
-    const newTask = {
-      id: data.length + 1,
-      task: taskInputText,
-      isCompleted: false,
-      isActive: true,
-    };
-    setData([...data, newTask]);
-    setTaskInputText("");
-  };
-  const checkTaskHandler = (taskId) => {
-    const updatedData = data.map((task) => {
-      if (task.id === taskId) {
-        console.log("task.id === taskId");
-        return { ...task, isCompleted: true };
-      }
-      return task;
-    });
-    setData(updatedData);
-  };
-  const deleteTask = (taskId) => {
-    const updatedData = data.map((task) => {
-      if (task.id === taskId) {
-        return { ...task, isActive: false };
-      }
-      return task;
-    });
-    setData(updatedData);
-    console.log(updatedData);
-  };
+
+  const data = useSelector((state) => state.todos)
+  const dispatch = useDispatch();
+
+  const handleAddTask = () => {
+    console.log(`taskInputText`, taskInputText);
+    dispatch(addTaskHandler({ taskInputText }));
+  }
+  const handleCheckTask = (taskId) => {
+    dispatch(checkTaskHandler({ taskId }))
+  }
+  const handleDeleteTask = (taskId) => {
+    dispatch(deleteTask({ taskId }));
+  }
+
+
   return (
     <div className="bg-zinc-700 px-4 py-6 m-9 rounded-2xl">
       <div className="flex justify-start">
@@ -68,7 +36,7 @@ function TodoBox() {
         />
         <button
           type="submit"
-          onClick={addTaskHandler}
+          onClick={handleAddTask}
           className="text-zinc-50 bg-green-500 px-5 py-0 rounded-xl mx-1"
         >
           Add
@@ -81,15 +49,15 @@ function TodoBox() {
               <div className="flex justify-start mt-5" key={idx}>
                 <input
                   type="checkbox"
-                  onChange={() => checkTaskHandler(taskItem.id)}
+                  onChange={() => handleCheckTask(taskItem.id)}
                   className="mx-1"
                 />
-                <p className={`text-zinc-100 ${taskItem.isCompleted && "line-through" }`} >
+                <p className={`text-zinc-100 ${taskItem.isCompleted && "line-through"}`} >
                   {taskItem.task}
                 </p>
                 <button
                   className="bg-red-500 text-zinc-100 px-4 rounded-xl mx-1"
-                  onClick={() => deleteTask(taskItem.id)}
+                  onClick={() => handleDeleteTask(taskItem.id)}
                 >
                   Delete
                 </button>
---
+++ pratice/todo_list/src/features/data/dataSlice.js
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = [
+  {
+    id: 1,
+    task: "Task One",
+    isCompleted: false,
+    isActive: true,
+  },
+  {
+    id: 2,
+    task: "Task Two",
+    isCompleted: false,
+    isActive: true,
+  },
+  {
+    id: 3,
+    task: "Task Three",
+    isCompleted: false,
+    isActive: true,
+  },
+];
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTaskHandler: (state, action) => {
+      const newTask = {
+        id: state.length + 1,
+        task: action.payload.taskInputText,
+        isCompleted: false,
+        isActive: true,
+      };
+      state.push(newTask);
+    },
+    checkTaskHandler: (state, action) => {
+      const task = state.find((task) => task.id === action.payload.taskId);
+      if (task) {
+        task.isCompleted = true;
+      }
+    },
+    deleteTask: (state, action) => {
+      const task = state.find((task) => task.id === action.payload.taskId);
+      if (task) {
+        task.isActive = false;
+      }
+    },
+  },
+});
+// Action creators are generated for each case reducer function
+export const { addTaskHandler, checkTaskHandler, deleteTask } =
+  todosSlice.actions;
+export default todosSlice.reducer;
