7da234aa5ceb5f6dc2fabbdab6499a8204b8da10
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-25 11:01:09 +0530

    "Removed default Vite and React logos, buttons, and text, and replaced with a TodoBox component and a new CSS file."

---
+++ pratice/todo_list/src/componets/TodoBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function TodoBox() {
+  return (
+    <div className=''>
+        
+        <div className='bg-zinc-700 px-20 py-40 m-9 w-full rounded-2xl'>
+
+        <input type="text" name="" id="" />
+        </div>
+
+        </div>
+    
+  )
+}
+
+export default TodoBox
