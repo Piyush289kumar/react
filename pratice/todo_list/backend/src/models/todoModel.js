d14fe4c5fab63ff098e44f131eb64981fccc4aa4
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-26 22:51:12 +0530

    "Added mongoose dependency, updated package-lock.json, and modified app.js to connect to MongoDB database"

--- pratice/todo_list/backend/src/models/todoModel.js
+++ pratice/todo_list/backend/src/models/todoModel.js
@@ -1,3 +1,22 @@
-import { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 
-const todoSchema = new Schema({}, { timestamps: true });
+const todoSchema = new Schema(
+  {
+    task: {
+      type: String,
+      trim: true,
+      index: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Todo = model("Todo", todoSchema);
