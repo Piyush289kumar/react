730eabf636b6faae488e62fb6cd3ba66be3f0ab0
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-29 22:48:22 +0530

    Form Data Save Into Database

--- pratice/user_form/backend/controllers/formController.js
+++ pratice/user_form/backend/controllers/formController.js
@@ -2,18 +2,20 @@ import Form from "../model/FormModels.js";
 const init = async (req, res) => {
   res.json("Server API init");
 };
+
 const formController = async (req, res) => {
-  res.json("Server API formController");
+  // Log the initial response for debugging purposes
+  console.log("Server API formController");
 
   const { firstName, lastName } = req.body;
+
+  // Check for missing fields and return early if any are missing
   if (!firstName || !lastName) {
-    res.status(400).json({ error: "Please fill in all fields" });
+    return res.status(400).json({ error: "Please fill in all fields" });
   }
 
-  res.status(200).json({ firstName, lastName });
-
-//   await Form.create({ firstName, lastName });
-
-//   res.status(200).json({ error: "Form Filled" });
+  await Form.create({ firstName, lastName });
+  res.status(200).json({ success: "Form Data Inserted..." });
 };
+
 export { init, formController };
