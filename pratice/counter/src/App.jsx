f4967d843943a5d81acc95df5d0741804e04b69e
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-30 22:30:33 +0530

    Function BUild

--- pratice/counter/src/App.jsx
+++ pratice/counter/src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [conter, setconter] = useState(0);
+
+  const increseHandler = () => {
+    setconter(conter + 1);
+  };
+  const decresHanlder = () => {
+    setconter(conter - 1);
+  };
 
   return (
     <>
-     <div>
-      
-     </div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <button>+</button>
+        <h1>1</h1>
+        <button>-</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
