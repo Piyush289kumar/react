1067d2b7c7eed01a575ec29c4d81c2c9026df526
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-02 21:34:01 +0530

    "Challenge 1 Finish"

--- challenge/show_hide_title/src/App.jsx
+++ challenge/show_hide_title/src/App.jsx
@@ -2,22 +2,33 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
 function App() {
-
-
+  const [isDisplay, setIsDisplay] = useState(true);
+  function toggleHandler() {
+    setIsDisplay(!isDisplay);
+  }
   return (
     <>
       <div className="container bg-zinc-900 h-screen w-full">
         <div className="flex flex-col items-center justify-center h-full">
           <div className="flex flex-col items-center justify-center text-zinc-300">
-            <h1 className="text-3xl mb-5 font-extrabold">Welcome to React Challenges</h1>
-            <button className="bg-green-500 font-bold text-2xl px-3 py-2 rounded-xl">Show/Hide</button>
+            <h1
+              className={`text-3xl mb-5 font-extrabold block ${
+                !isDisplay && "text-transparent"
+              }`}
+            >
+              Welcome to React Challenges
+            </h1>
+            <button
+              onClick={toggleHandler}
+              className="bg-green-500 font-bold text-2xl px-3 py-2 rounded-xl"
+            >
+              Show/Hide
+            </button>
           </div>
         </div>
       </div>
     </>
   );
 }
-
 export default App;
